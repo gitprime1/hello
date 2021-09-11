82c882ca1978b45ba50d48b25fc43c4eda8d1919
gitprime1 <87187748+gitprime1@users.noreply.github.com>
2021-09-11 23:00:08 +0530

    Update max_sub_array.py

--- max_sub_array.py
+++ max_sub_array.py
@@ -21,12 +21,13 @@ def function(A:[],low,high):
 
       return max(function(A,low,mid),function(A,mid + 1,high),merge(A,low,mid,high))
 
+
 n = int(input("the size of the array"))
 optimus = []
 i = 0
 for i in range(n):
     z = int(input("enter the number"))
-    Array.append(z)
-print(Array)
-    
-print(function(optmius, 0, len(optmius) - 1))
+    optimus.append(z)
+print(optimus)
+
+print(function(optimus, 0, len(optimus) - 1))
