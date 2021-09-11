aade607bddbea2037a11604c3325f118569a362d
gitprime1 <87187748+gitprime1@users.noreply.github.com>
2021-09-11 23:28:41 +0530

    Update max_sub_array.py

--- max_sub_array.py
+++ max_sub_array.py
@@ -1,14 +1,28 @@
- def merge(A, l, h):
+def merge(A: [], l, m, h):
     sum = 0
-    for i in range(l, h +1):
-       sum += A[i]
-    return sum
-    def function(A: [], low, high):
-        if low == high:
-            return A[low]
-        else:
-            mid = (low + high) // 2
-            return max(function(A, low, mid), function(A, mid + 1, high),merge(A,low,high)) 
+    left_sum = -1000000
+    for i in range(m, l - 1, -1):
+        sum = sum + A[i]
+        if (sum > left_sum):
+            left_sum = sum
+    sm = 0
+    right_sum = -100000
+    for i in range(m + 1, h + 1):
+        sm += A[i]
+        if sm > right_sum:
+            right_sum = sm
+    return left_sum + right_sum
+
+
+def function(A: [], low, high):
+    if low == high:
+        return A[low]
+    else:
+        mid = (low + high) // 2
+
+        return max(function(A, low, mid), function(A, mid + 1, high), merge(A, low, mid, high))
+
+
 n = int(input("the size of the array"))
 optimus = []
 i = 0
