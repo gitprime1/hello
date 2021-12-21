9eb5df10698d8624664960ec24b4395c49505c4f
gitprime2 <87187748+gitprime2@users.noreply.github.com>
2021-12-21 23:21:12 +0530

    commit changes

--- leftrotate.py
+++ leftrotate.py
@@ -1,4 +1,5 @@
 def rotateLeft(d, arr):
+    #it  required n times
     # Write your code here
     num = 0
     list1 = []
@@ -9,6 +10,18 @@ def rotateLeft(d, arr):
             arr = list1[:]
         list1 = arr[1:] + [arr[0]]
         num = num + 1
+    return list1
+print(rotateLeft(54,[1,2,3,4,5]))
+print(rotateLeft(32,[9,5,4,3,2,1]))
+#optimized code for the above
+def rotateLeft(d, arr):
+    # Write your code here
+    #it required 1 time
+    num = 0
+    list1 = []
+    num1 = len(arr)
+    d = divmod(d,len(arr))[1]
+    list1 = arr[d:] + arr[:d]
     return list1
 print(rotateLeft(54,[1,2,3,4,5]))
 print(rotateLeft(32,[9,5,4,3,2,1]))
